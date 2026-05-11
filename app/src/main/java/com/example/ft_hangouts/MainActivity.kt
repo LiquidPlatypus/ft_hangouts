3eefdbc62af5a90c06f3509580a04444b9b97509
Liquid Platypus <104024934+LiquidPlatypus@users.noreply.github.com>
2026-05-11 14:59:29 +0200

    contact view

--- app/src/main/java/com/example/ft_hangouts/MainActivity.kt
+++ app/src/main/java/com/example/ft_hangouts/MainActivity.kt
@@ -4,14 +4,19 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+	private lateinit var contactAdapter: ContactAdapter
+	private lateinit var dbHelper: ContactsDbHelper
+
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		enableEdgeToEdge()
@@ -25,6 +30,23 @@ class MainActivity : AppCompatActivity() {
 			v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
 			insets
 		}
+
+		dbHelper = ContactsDbHelper(this)
+
+		val recyclerView = findViewById<RecyclerView>(R.id.recycler_contacts)
+		recyclerView.layoutManager = LinearLayoutManager(this)
+		contactAdapter = ContactAdapter(emptyList())
+		recyclerView.adapter = contactAdapter
+	}
+
+	override fun onResume() {
+		super.onResume()
+		loadContacts()
+	}
+
+	private fun loadContacts() {
+		val contacts = dbHelper.getAllContacts()
+		contactAdapter.updateData(contacts)
 	}
 
 	override fun onCreateOptionsMenu(menu: Menu): Boolean {
