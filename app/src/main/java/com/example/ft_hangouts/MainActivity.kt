70392e4a00035694f95bb1f3bb94f555160d5b78
Liquid Platypus <104024934+LiquidPlatypus@users.noreply.github.com>
2026-05-13 15:04:00 +0200

    header color picker

--- app/src/main/java/com/example/ft_hangouts/MainActivity.kt
+++ app/src/main/java/com/example/ft_hangouts/MainActivity.kt
@@ -11,6 +11,11 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import android.content.Context
+import android.graphics.Color
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.ColorUtils
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
 
@@ -24,6 +29,17 @@ class MainActivity : AppCompatActivity() {
 
 		val toolbar = findViewById<Toolbar>(R.id.toolbar)
 		setSupportActionBar(toolbar)
+
+		applyHeaderColorFromPrefs()
+
+		supportFragmentManager.setFragmentResultListener(
+			ColorPickerDialogFragment.RESULT_KEY, this
+		) { _, bundle ->
+			val color = bundle.getInt("color")
+			getSharedPreferences("settings", Context.MODE_PRIVATE)
+				.edit().putInt("header_color", color).apply()
+			applyHeaderColorFromPrefs()
+		}
 
 		ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
 			val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -41,12 +57,34 @@ class MainActivity : AppCompatActivity() {
 
 	override fun onResume() {
 		super.onResume()
+		applyHeaderColorFromPrefs()
 		loadContacts()
 	}
 
 	private fun loadContacts() {
 		val contacts = dbHelper.getAllContacts()
 		contactAdapter.updateData(contacts)
+	}
+
+	private fun applyHeaderColorFromPrefs() {
+		val prefs = getSharedPreferences("settings", Context.MODE_PRIVATE)
+		val defaultColor = ContextCompat.getColor(this, R.color.header_background)
+		val color = prefs.getInt("header_color", defaultColor)
+
+		val toolbar = findViewById<androidx.appcompat.widget.Toolbar>(R.id.toolbar)
+		toolbar.setBackgroundColor(color)
+
+		val textColor = if (ColorUtils.calculateLuminance(color) > 0.5) Color.BLACK else Color.WHITE
+		toolbar.setTitleTextColor(textColor)
+		toolbar.navigationIcon?.setTint(textColor)
+		for (i in 0 until toolbar.menu.size()) {
+			toolbar.menu.getItem(i).icon?.setTint(textColor)
+		}
+	}
+
+	private fun showHSVColorPicker() {
+		val frag = ColorPickerDialogFragment.newInstance()
+		frag.show(supportFragmentManager, "color_picker")
 	}
 
 	override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -59,6 +97,10 @@ class MainActivity : AppCompatActivity() {
 			R.id.action_add_contact -> {
 				val intent = Intent(this, AddContactActivity::class.java)
 				startActivity(intent)
+				true
+			}
+			R.id.action_change_header -> {
+				showHSVColorPicker()
 				true
 			}
 			else -> super.onOptionsItemSelected(item)
